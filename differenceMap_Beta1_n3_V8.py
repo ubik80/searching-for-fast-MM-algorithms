03b3007a7e7ad175adb922996f97cb2980b39877
Till Späth <tillspaeth@web.de>
2019-05-30 19:26:58 +0200

    V8

--- differenceMap_Beta1_n3_V8.py
+++ differenceMap_Beta1_n3_V8.py
@@ -19,32 +19,8 @@ def PB(W,id):
     p=W[0].shape[0]
     nn=W[0].shape[1]
     n=int(np.sqrt(nn))
-    oldDiff=99999.9
-    solved=False
-    for i in range(1):
-        Wa=W[0].copy()
-        Wb=W[1].copy()
-        Wc=W[2].copy()
-        Wa,Wb,Wc,eh,s=bi.findCalcRule(n,p,3000000,Wa,Wb,Wc,limit=0.01,nue=0.1)
-        diff=np.linalg.norm(Wa-W[0],2)**2+np.linalg.norm(Wb-W[1],2)**2+np.linalg.norm(Wc-W[2],2)**2
-        if s and diff<oldDiff:
-            oldDiff=diff
-            WaRet=Wa.copy()
-            WbRet=Wb.copy()
-            WcRet=Wc.copy()
-            solved=True
-    if solved: W=[WaRet,WbRet,WcRet]
-    else:
-        s=False
-        while not s:
-            print("   Prz: ",id," BP failed -> reset")
-            seed=int(time.time())+int(uuid.uuid4())+id
-            np.random.seed(seed%135745)
-            W=[np.random.rand(p*nn).reshape([p,nn])*2.0-1.0,
-            np.random.rand(p*nn).reshape([p,nn])*2.0-1.0,
-            np.random.rand(nn*p).reshape([nn,p])*2.0-1.0]
-            W[0],W[1],W[2],eh,s=bi.findCalcRule(n,p,3000000,W[0],W[1],W[2],limit=0.01,nue=0.1)
-    return W,not solved #PB
+    W[0],W[1],W[2],eh,success=bi.findCalcRule(n,p,3000000,W[0],W[1],W[2],limit=0.01,nue=0.1)
+    return W,success #PB
 
 def checkSolution(W):
     p=W[0].shape[0]
@@ -105,22 +81,27 @@ def diffMap(id,mutex,success):
 
     i=0
     while success.value==0:
-        PBx,failed=PB(W.copy(),id)
+        s=False
+        while not s:
+            PBx,s=PB(W.copy(),id)
+            if not s:
+                print("   Prz: ",id," BP failed -> reset")
+                seed=int(time.time())+int(uuid.uuid4())+id
+                np.random.seed(seed%135745)
+                W=[np.random.rand(p*nn).reshape([p,nn])*2.0-1.0,
+                np.random.rand(p*nn).reshape([p,nn])*2.0-1.0,
+                np.random.rand(nn*p).reshape([nn,p])*2.0-1.0]
+                i=0
+
         PAy=PA([2.0*PBx[0]-W[0],2.0*PBx[1]-W[1],2.0*PBx[2]-W[2]])
         delta=[PAy[0]-PBx[0],PAy[1]-PBx[1],PAy[2]-PBx[2]]
+        W=[W[0]+delta[0],W[1]+delta[1],W[2]+delta[2]]
         norm2Delta=np.linalg.norm(delta[0],2)**2+np.linalg.norm(delta[1],2)**2+np.linalg.norm(delta[2],2)**2
         norm2Delta=np.sqrt(norm2Delta)
-        W=[W[0]+delta[0],W[1]+delta[1],W[2]+delta[2]]
 
         cycle=BF.store(PAy)
-        if cycle:
-            W[0]+=np.random.rand(p*nn).reshape([p,nn])-0.5
-            W[1]+=np.random.rand(p*nn).reshape([p,nn])-0.5
-            W[2]+=np.random.rand(p*nn).reshape([nn,p])-0.5
 
         mutex.acquire()
-        if cycle:
-            print("**** Zyklus entdeckt! *****")
         if i%1==0:
             print("---------------------------")
             print("Prozess:",id)
@@ -136,22 +117,24 @@ def diffMap(id,mutex,success):
                 np.random.rand(p*nn).reshape([p,nn])*2.0-1.0,
                 np.random.rand(nn*p).reshape([nn,p])*2.0-1.0]
                 i=0
-                #success.value=1
-                #mutex.release()
-                #return #diffMap
             else: print(".... keine gültige Lösung")
+        if cycle:
+            print("**** Zyklus entdeckt! *****")
         if i==1000:
             print(i," cycles -> Reset")
-
         mutex.release()
 
-        i+=1
-        if failed: i=0
+        if cycle:
+            W[0]+=np.random.rand(p*nn).reshape([p,nn])-0.5
+            W[1]+=np.random.rand(p*nn).reshape([p,nn])-0.5
+            W[2]+=np.random.rand(p*nn).reshape([nn,p])-0.5
+
         if i==1000:
             W=[np.random.rand(p*nn).reshape([p,nn])*2.0-1.0,
             np.random.rand(p*nn).reshape([p,nn])*2.0-1.0,
             np.random.rand(nn*p).reshape([nn,p])*2.0-1.0]
             i=0
+        i+=1
     return #diffMap
 
 if __name__ == '__main__':
