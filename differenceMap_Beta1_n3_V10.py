f3bb4c860d0a75bd00cdb191342870d45a7bdfa1
Till Späth <tillspaeth@web.de>
2019-06-14 17:37:26 +0200

    ...

--- differenceMap_Beta1_n3_V10.py
+++ differenceMap_Beta1_n3_V10.py
@@ -150,7 +150,7 @@ def diffMap(id,mutex,success):
         if cycle:
             print("**** Zyklus entdeckt! *****")
             print("**** cyclCnt: ",cyclCnt)
-        if i==4000:
+        if i==2000:
             print(i," cycles -> Reset")
         mutex.release()
 
@@ -159,7 +159,7 @@ def diffMap(id,mutex,success):
             W[1]+=(np.random.rand(p*nn).reshape([p,nn])*2.0-1.0)*0.05*cyclCnt
             W[2]+=(np.random.rand(p*nn).reshape([nn,p])*2.0-1.0)*0.05*cyclCnt
 
-        if i==4000:
+        if i==2000:
             W,startVals=init(startVals)
             i=0
             BF=bf.bloomFilter(2*nn*p,0.00001)
